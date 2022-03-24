a5adee4d851fef92eebd31c5865f74e9a4d50fb9
madhumithaanbarasan <73183853+madhumithaanbarasan@users.noreply.github.com>
2022-03-24 12:04:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function compute()
+
+    function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    results=document.getElementById("result");
     
+    result.innerHTML=(p*r*t)/100;
+    
+
 }
         
